68e23dcaf8b003c7a5a696568a6e23b9826d82f6
kennylam91 <phamvanlam1991@gmail.com>
2019-04-25 11:45:34 +0700

    add file thuchanh4

---
+++ thuchanh4.js
@@ -0,0 +1,10 @@
+let number=parseFloat(prompt("Đây là chương trình kiểm tra số nhập vào là số âm hay dương. Nếu âm thì hiển thị -1, dương hiển thị 1, còn lại 0. Nhập vào số cầnkiểm tra: "));
+if(number<0){
+    alert("-1");
+}
+else if(number>0){
+    alert("1");
+}
+else{
+    alert("0");
+}
