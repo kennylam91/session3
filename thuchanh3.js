ab7c8c499e99bafe769663ad310542c4bb105d94
kennylam91 <phamvanlam1991@gmail.com>
2019-04-25 11:31:46 +0700

    add file bai3

---
+++ thuchanh3.js
@@ -0,0 +1,20 @@
+let username = prompt("Username= ");
+if (username) {
+    if (username == "Admin") {
+        let password = prompt("Password= ");
+        if (password) {
+            if (password == "TheMaster") {
+                alert("Welcome!");
+            } else {
+                alert("Wrong password");
+            }
+
+        } else {
+            alert("Chua nhap password");
+        }
+
+    }
+} else {
+    alert("Chua nhap username")
+}
+
