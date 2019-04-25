7ef77bb938ddac19df6889e8b63564cf936d8e28
kennylam91 <phamvanlam1991@gmail.com>
2019-04-25 10:54:36 +0700

    add bai 2

---
+++ thuchanh2.js
@@ -0,0 +1,7 @@
+let answer = prompt('What is the "official" name of JavaScipt?');
+if(answer=="ECMAScript") {
+    document.write("Right!");
+}
+else{
+    document.write("Didn't know? ECMAScript!");
+}
